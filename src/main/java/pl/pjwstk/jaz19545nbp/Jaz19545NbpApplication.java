8aad05671677dba9d79c9a4eff4e5f37f3599697
Falron98 <bartekkrystowski@wp.pl>
2021-07-03 16:13:09 +0200

    Start

---
+++ src/main/java/pl/pjwstk/jaz19545nbp/Jaz19545NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.jaz19545nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz19545NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz19545NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjwstk/jaz19545nbp/Jaz19545NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjwstk.jaz19545nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz19545NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
